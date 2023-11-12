f7b056486ad63c8263ecb48e09d8b0a7159474c2
olaide <olaideabiola2@gmail.com>
2023-11-12 18:34:54 +0100

    edit

--- Routes/items.js
+++ Routes/items.js
@@ -76,7 +76,7 @@ router.get("/by-id/:id", async (req, res) => {
     const note = await itemsCollection.findById(id);
   
     if(req.decoded.userId != note.user) {
-      res.status(401).send("You are not allowed to delete this task");
+      console.log("hello world");
       return;
     }
   console.log("hello world");
